05bdac9c95bf99ca492d2685487f246d4ce7e706
Antonio Lopez <alopez@rti.com>
2018-07-20 13:43:45 +0200

    Updated build system, we need to fix the issues of use a build system in two levels #4

---
+++ resources/cmake/ConnextDdsRosDdsTypes.cmake
@@ -0,0 +1,33 @@
+include(ConnextDdsCodegen)
+
+macro(connextdds_generate_ros_dds_types)
+    set(options UNBOUNDED)
+    set(single_value_args LANG OUTPUT_DIRECTORY 
+        OBJECT_LIBRARY_NAME SOURCES 
+    )
+    set(multi_value_args IDL_FILES INCLUDE_DIRS)
+
+    cmake_parse_arguments(_ROS_TYPES
+        "${options}"
+        "${single_value_args}"
+        "${multi_value_args}"
+        ${ARGN}
+    )
+
+    foreach(file ${_ROS_TYPES_IDL_FILES})
+        connextdds_rtiddsgen_run(
+            LANG ${_ROS_TYPES_LANG}
+            OUTPUT_DIRECTORY ${_ROS_TYPES_OUTPUT_DIRECTORY}
+            IDL_FILE ${file}
+            INCLUDE_DIRS ${_ROS_TYPES_INCLUDE_DIRS}
+            VAR generated_file
+        )
+
+        list(APPEND idl_list_source 
+            ${generated_file_CXX11_SOURCES} 
+            ${generated_file_CXX11_HEADERS}
+        )
+
+    endforeach()
+    
+endmacro()
